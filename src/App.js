b1707916ef74a75c1bdc4309780de4bfba58baa3
Davut Dayan <dayan.davut@gmail.com>
2019-12-25 19:36:10 +0100

    Clean-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import GlobalStyles from './GlobalStyles'
-import ClubList from './ClubList'
+import ClubList from './clubs/ClubList'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import ClubOverview from './ClubOverview'
+import ClubOverview from './clubs/ClubOverview'
 import { getClubs, postClub } from './services.js'
-import NewClub from './NewClub'
+import NewClub from './clubs/NewClub'
 
 export default function App() {
   const [clubs, setClubs] = useState([])
